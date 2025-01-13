0588434652d66d0365946f20e7e438a89586f6ad
poppuko-n <souta0609@icloud.com>
2025-01-13 12:56:37 +0900

    companyクラス責務の分離

--- my_api_app/app/models/stock_price.rb
+++ my_api_app/app/models/stock_price.rb
@@ -4,11 +4,5 @@ class StockPrice < ApplicationRecord
   validates :company_code, presence: true
   validates :date, presence: true
   validates :close_price, presence: true
-
-  def self.latest_two_for_company(company_code)
-    StockPrice.where(company_code: company_code)
-    .order(date: :desc)
-    .limit(2)
-    .pluck(:close_price)
-  end
+  
 end
