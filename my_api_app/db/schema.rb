1e983b9440a0156dcaeb159cf5cb6efcef466233
poppuko-n <souta0609@icloud.com>
2025-01-18 22:53:40 +0900

    カテゴリテーブル作成

--- my_api_app/db/schema.rb
+++ my_api_app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_02_053911) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_18_103208) do
+  create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", limit: 50, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
   create_table "companies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "code", null: false
     t.integer "sector_id", null: false
