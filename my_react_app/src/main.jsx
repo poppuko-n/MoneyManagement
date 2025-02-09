0558b2fba011b6b6a8ce5e673cb79d1181a5be8a
poppuko-n <souta0609@icloud.com>
2025-02-09 13:00:55 +0900

    ログインと会員登録のモーダル表示

--- my_react_app/src/main.jsx
+++ my_react_app/src/main.jsx
@@ -1,18 +1,42 @@
 import { StrictMode } from "react";
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Header from "./Header.jsx";
 import Navigation from "./Navigation.jsx";
+import LoginForm from "./LoginForm.jsx";
+import SignUp from "./SignUp.jsx";
+import Modal from "./Modal.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <Navigation />
-      <main className="flex-grow">
-        <App />
-      </main>
-    </div>
-  </StrictMode>
-);
+const AppContainer = () => {
+
+  const [isLogin, setIsLogin] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  return (
+    <StrictMode>
+      <div className="flex flex-col min-h-screen">
+        <Header setIsLogin={setIsLogin} setIsSignUp={setIsSignUp} />
+
+        {isLogin && (
+          <Modal onClose={() => setIsLogin(false)}>
+            <LoginForm />
+          </Modal>
+        )}
+        {isSignUp && (
+          <Modal onClose={() => setIsSignUp(false)}>
+            <SignUp />
+          </Modal>
+        )}
+
+        <Navigation />
+        <main className="flex-grow">
+          <App />
+        </main>
+      </div>
+    </StrictMode>
+  );
+};
+
+createRoot(document.getElementById("root")).render(<AppContainer />);
