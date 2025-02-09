4ece5f70d6f0044b1a875e16d3c697f4058ecca2
poppuko-n <souta0609@icloud.com>
2025-02-09 23:55:04 +0900

    レイアウト修正

--- my_react_app/src/Header.jsx
+++ my_react_app/src/Header.jsx
@@ -1,24 +1,33 @@
 import logoImage from "./assets/logo.png";
 
-const Header = ({setIsLogin, setIsSignUp }) => {
+const Header = ({ setIsLogin, setIsSignUp }) => {
   return (
-    <header className="bg-white shadow-md">
+    <header className="bg-white mb-6">
       <div className="container mx-auto flex items-center justify-between py-4 px-6">
         <div className="flex items-center">
-          <img src={logoImage}  alt="Logo" className="h-10" />
-          <a href="/"className="ml-2 text-xl font-bold text-gray-800">MoneyManagement</a>
+          <img src={logoImage} alt="Logo" className="h-10 mr-2" />
+          <a href="/" className="ml-2 text-2xl font-bold text-gray-800 tracking-wide">
+            <span >Money</span>
+            <span className="text-green-600">Management</span>
+          </a>
         </div>
         <div className="hidden md:flex items-center space-x-4">
-          <button 
+          <button
             className="text-gray-600 hover:text-green-500"
-            onClick={()=>{setIsLogin(true)}}
-            >
-              ログイン</button>
-          <button 
+            onClick={() => {
+              setIsLogin(true);
+            }}
+          >
+            ログイン
+          </button>
+          <button
             className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-            onClick={()=>{setIsSignUp(true)}}
-            >
-              新規会員登録</button>
+            onClick={() => {
+              setIsSignUp(true);
+            }}
+          >
+            新規会員登録
+          </button>
         </div>
       </div>
     </header>
