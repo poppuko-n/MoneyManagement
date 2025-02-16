54b830847a87d14623723f176b7324d287d34b6e
poppuko-n <souta0609@icloud.com>
2025-02-16 15:38:24 +0900

    S3の404対策（HashVrowser）

--- my_react_app/src/main.jsx
+++ my_react_app/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import Header from "./Header.jsx";
 import Navigation from "./Navigation.jsx";
 import LoginForm from "./LoginForm.jsx";
@@ -31,9 +30,7 @@ const AppContainer = () => {
         )}
 
         <Navigation />
-        <main className="flex-grow">
-          <App />
-        </main>
+
       </div>
     </StrictMode>
   );
