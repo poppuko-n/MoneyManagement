4e067fbcdc096a8074394e341034c166a7fbbf0e
poppuko-n <souta0609@icloud.com>
2025-03-16 20:33:16 +0900

    importメソッドへ修正しバリデーションを適用させる

--- my_api_app/app/models/stock_price/importer.rb
+++ my_api_app/app/models/stock_price/importer.rb
@@ -4,7 +4,7 @@ class StockPrice
       def call
         StockPrice::WeeklyStockFetcher::TARGET_CODES.each do |code|
           stock_prices = format_stock_prices(StockPrice::WeeklyStockFetcher.call(code))
-          StockPrice.upsert_all(stock_prices, on_duplicate: :update)
+          StockPrice.import stock_prices, on_duplicate_key_update: [:close_price]
         end
       end
 
@@ -12,11 +12,11 @@ class StockPrice
 
       def format_stock_prices(quotes)
         quotes.map do |quote|
-          {
+          StockPrice.new(
             company_code: quote["Code"].to_i,
             date: quote["Date"],
             close_price: quote["Close"]
-          }
+          )
         end
       end
     end
