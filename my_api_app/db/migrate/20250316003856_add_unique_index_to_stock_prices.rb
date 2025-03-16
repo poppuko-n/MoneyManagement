5888e5134f9a4c7b165fb022b0d31271ad59206f
poppuko-n <souta0609@icloud.com>
2025-03-16 19:28:45 +0900

    マイグレーションファイルのインデント調整

--- my_api_app/db/migrate/20250316003856_add_unique_index_to_stock_prices.rb
+++ my_api_app/db/migrate/20250316003856_add_unique_index_to_stock_prices.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToStockPrices < ActiveRecord::Migration[8.0]
   def change
-    add_index :stock_prices, [:company_code, :date], unique: true, name: "index_stock_prices_on_company_code_and_date"
+    add_index :stock_prices, [ :company_code, :date ], unique: true, name: "index_stock_prices_on_company_code_and_date"
   end
 end
