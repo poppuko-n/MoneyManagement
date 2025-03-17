c70e0cbd748d91cdaa910c907bba7cae4feb4740
poppuko-n <souta0609@icloud.com>
2025-03-17 21:22:04 +0900

    バリデーションエラーログの出力機能を追加

--- my_api_app/app/models/stock_price/importer.rb
+++ my_api_app/app/models/stock_price/importer.rb
@@ -1,11 +1,13 @@
 class StockPrice
   module Importer
+    BASE_SIZE = 1000
+
     class << self
       def call
-        StockPrice::WeeklyStockFetcher::TARGET_CODES.each do |code|
-          stock_prices = format_stock_prices(StockPrice::WeeklyStockFetcher.call(code))
-          StockPrice.import stock_prices, on_duplicate_key_update: [:close_price]
+        stock_prices = StockPrice::WeeklyStockFetcher::TARGET_CODES.flat_map do |code|
+           format_stock_prices(StockPrice::WeeklyStockFetcher.call(code))
         end
+        import_with_logging(stock_prices)
       end
 
       private
@@ -18,6 +20,18 @@ class StockPrice
             close_price: quote["Close"]
           )
         end
+      end
+
+      def import_with_logging(stock_prices)
+        stock_prices.each_slice(BASE_SIZE) do |batch|
+          valid_stprice, unvalid_stprice = batch.partition(&:valid?)
+          log_errors(unvalid_stprice) unless unvalid_stprice.empty?
+          StockPrice.import valid_stprice, on_duplicate_key_update: [:close_price]
+        end
+      end
+
+      def log_errors(records)
+        records.each {|record| puts record.errors.full_messages}
       end
     end
   end
