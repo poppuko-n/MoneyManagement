f9ffe5a50277c04c480dc7e427f912afa3ea5406
poppuko-n <souta0609@icloud.com>
2025-03-18 11:51:05 +0900

    Userテーブルの作成

---
+++ my_api_app/db/migrate/20250318024833_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false, limit: 50
+      t.string :password, null: false, limit: 50
+
+      t.timestamps
+    end
+  end
+end
