2c27594d5c719ff8aa05f3abc5f3b21c0241cb65
poppuko-n <souta0609@icloud.com>
2025-03-20 00:42:04 +0900

    ログイン処理の実装

--- my_api_app/app/controllers/users_controller.rb
+++ my_api_app/app/controllers/users_controller.rb
@@ -3,15 +3,25 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       token = create_token(user.id)
-      render json: {token: token}, status: :created
+      render json: { token: token }, status: :created
     else
       render json: { user: user, errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+      token = create_token(user.id)
+      render json: { token: token }, status: :ok
+    else
+      render json: { user: user, errors: user.errors.full_messages }, status: :unauthorized
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
