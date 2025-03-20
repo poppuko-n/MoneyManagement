96b2b1aa5acb800d79e90566ae80d09abadfaa51
poppuko-n <souta0609@icloud.com>
2025-03-20 18:33:09 +0900

    categoryテーブルの初期設定

--- my_api_app/db/seeds.rb
+++ my_api_app/db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+TransactionCategory.create!([
+  { name: "給与", transaction_type: 0 },
+  { name: "副業", transaction_type: 0 },
+  { name: "その他", transaction_type: 0 },
+  { name: "食費", transaction_type: 1 },
+  { name: "日用品", transaction_type: 1 },
+  { name: "交際費", transaction_type: 1 },
+  { name: "交通費", transaction_type: 1 },
+  { name: "固定費", transaction_type: 1 }
+])
