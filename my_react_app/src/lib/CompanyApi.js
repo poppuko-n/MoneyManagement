baf4d9c87706bafcc885a83c56d698833a68d2e0
poppuko-n <souta0609@icloud.com>
2025-03-22 22:41:10 +0900

    ComapnyApiの作成

---
+++ my_react_app/src/lib/CompanyApi.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+class CompanyApi {
+  static apiBaseUrl = window.env?.API_BASE_URL || "http://localhost:3000"
+
+  static getCompanies() {
+    return axios
+      .get(`${this.apiBaseUrl}/companies`)
+      .catch(error => {
+        alert("データの取得に失敗しました。")
+      })
+  }
+}
+
+export default CompanyApi;
