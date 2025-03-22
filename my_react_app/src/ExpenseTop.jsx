62d3ea7070ed8560137a8e3bb2b5f04ba6b1a3c1
poppuko-n <souta0609@icloud.com>
2025-03-22 21:05:27 +0900

    ExpenseTopの新規作成

---
+++ my_react_app/src/ExpenseTop.jsx
@@ -0,0 +1,51 @@
+import addImage from "./assets/add.svg";
+
+const ExpenseTop = ({expenses, onCreateNew}) => {
+  const incomeTotal = expenses
+    .filter((expense) => expense.transaction_type === "収入")
+    .reduce((total, expense) => total + expense.amount, 0);
+
+  const expenseTotal = expenses
+    .filter((expense) => expense.transaction_type === "支出")
+    .reduce((total, expense) => total + expense.amount, 0);
+
+  const balance = incomeTotal - expenseTotal;
+
+  return (
+    <div className="container mx-auto p-4">
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="text-2xl border-b border-black pb-2 inline-block">月次収支</h1>
+        <div
+          onClick={onCreateNew}
+          className="cursor-pointer hover:bg-red-300 bg-red-400 rounded-full p-4 transition duration-700 flex items-center justify-center"
+        >
+          <img 
+            src={addImage} 
+            alt="add"
+            className="w-6 h-6" 
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-around items-center bg-gray-100 p-4 rounded shadow mb-4 text-center">
+        <div>
+          <p className="text-sm text-gray-600">収入</p>
+          <p className="text-3xl text-green-600">¥{incomeTotal.toLocaleString()}</p>
+        </div>
+        <p className="text-xl font-bold text-gray-600">-</p>
+        <div>
+          <p className="text-sm text-gray-600">支出</p>
+          <p className="text-3xl text-red-600">¥{expenseTotal.toLocaleString()}</p>
+        </div>
+        <p className="text-xl font-bold text-gray-600">=</p>
+        <div>
+          <p className="text-sm text-gray-600">収支</p>
+          <p className="text-3xl text-blue-600">¥{balance.toLocaleString()}</p>
+        </div>
+      </div>
+
+    </div> 
+  );
+};
+
+export default ExpenseTop;
