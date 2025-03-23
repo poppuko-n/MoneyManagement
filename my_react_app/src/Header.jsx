b7c5a803cbe681bfbc744a989358bed58e20b8b6
poppuko-n <souta0609@icloud.com>
2025-03-23 20:31:38 +0900

    feat: ユーザー作成時にAPIレスポンスからトークンを取得して保存する処理を追加

--- my_react_app/src/Header.jsx
+++ my_react_app/src/Header.jsx
@@ -1,34 +1,53 @@
 import logoImage from "./assets/logo.png";
 
 const Header = ({ setIsSignIN, setIsSignUp }) => {
+  const isLoggedIn = !!localStorage.getItem('token');
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    alert("ログアウトしました");
+  };
+  
   return (
     <header className="bg-white mb-2">
       <div className="container mx-auto flex items-center justify-between py-4 px-6">
         <div className="flex items-center">
           <img src={logoImage} alt="Logo" className="h-10 mr-2" />
           <a href="/" className="ml-2 text-2xl font-bold text-gray-800 tracking-wide">
-            <span >Money</span>
+            <span>Money</span>
             <span className="text-green-600">Management</span>
           </a>
         </div>
-        <div className="hidden md:flex items-center space-x-4">
+
+        {!isLoggedIn && (
+          <div className="hidden md:flex items-center space-x-4">
+            <button
+              className="text-gray-600 hover:text-green-500"
+              onClick={() => {
+                setIsSignIN(true);
+              }}
+            >
+              ログイン
+            </button>
+            <button
+              className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+              onClick={() => {
+                setIsSignUp(true);
+              }}
+            >
+              新規会員登録
+            </button>
+          </div>
+        )}
+        {isLoggedIn && (
           <button
-            className="text-gray-600 hover:text-green-500"
-            onClick={() => {
-              setIsSignIN(true);
-            }}
-          >
-            ログイン
-          </button>
-          <button
-            className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-            onClick={() => {
-              setIsSignUp(true);
-            }}
-          >
-            新規会員登録
-          </button>
-        </div>
+          className="text-gray-600 hover:text-green-500"
+          onClick={() => {
+            handleLogout();
+          }}
+        >
+          ログアウト
+        </button>
+        )}
       </div>
     </header>
   );
