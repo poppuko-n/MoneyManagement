96403d0082f07998d8d28bc09b7fa06cc423c6ff
poppuko-n <souta0609@icloud.com>
2025-03-24 21:45:03 +0900

    feat: authenticate_userを追加しログイン中であることを判定する

--- my_api_app/app/controllers/application_controller.rb
+++ my_api_app/app/controllers/application_controller.rb
@@ -1,8 +1,36 @@
 class ApplicationController < ActionController::API
+
+  def authenticate_user
+    authorization_header = request.headers[:Authorization]
+    unless authorization_header
+      return render_unauthorized('ログインが必要です。')
+    end
+
+    token = authorization_header.split(" ")[1]
+    secret_key = Rails.application.credentials.secret_key_base
+
+    begin
+      decoded_code = JWT.decode(token, secret_key)
+      @current_user = User.find(decoded_code[0]["user_id"])
+    rescue
+      ActiveRecord::RecordNotFound
+      render_unauthorized('ユーザーが見つかりません。')
+    rescue JWT::DecodeError => e
+      render_unauthorized("#{e.message}")
+    end
+
+  end
+
   def create_token(user_id)
     payload = { user_id: user_id }
     secret_key = Rails.application.credentials.secret_key_base
     token = JWT.encode(payload, secret_key)
     token
   end
+
+  private
+  def render_unauthorized(message = 'Unauthorized')
+    render json: { errors: [message] }, status: :unauthorized
+  end
+
 end
