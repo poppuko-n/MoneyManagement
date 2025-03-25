b80f6fdebe7bf43abd415f4f9dff2fab09b495db
poppuko-n <souta0609@icloud.com>
2025-03-25 22:37:55 +0900

    feat: expense_logにuser_idを紐付けて、ログインユーザーごとの登録・取得に対応

--- my_react_app/vite.config.js
+++ my_react_app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",  // ✅ ここを追加！S3 での 404 対策
+  base: "./", 
   plugins: [react()],
 })
