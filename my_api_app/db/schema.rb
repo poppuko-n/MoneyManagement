4ec7dd3baaec662f5888a143ee315abb6454a2a8
poppuko-n <souta0609@icloud.com>
2025-04-01 21:41:53 +0900

    fix: Userモデルのnameにユニーク制約を付与

--- my_api_app/db/schema.rb
+++ my_api_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_25_131244) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_01_123801) do
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", limit: 50, null: false
     t.datetime "created_at", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_25_131244) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "expense_logs", "categories"
