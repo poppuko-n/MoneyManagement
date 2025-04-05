e814d8abc717237ff27fa59cf3785f5e379dc17a
poppuko-n <souta0609@icloud.com>
2025-04-05 19:58:39 +0900

    style: ボタン表内容を修正

--- my_react_app/src/SelectCompanyHeader.jsx
+++ my_react_app/src/SelectCompanyHeader.jsx
@@ -11,9 +11,10 @@ const SelectCompanyHeader = ({ totalCost, onSubmit }) => {
       <h1 className="text-2xl border-b border-black pb-2 inline-block">銘柄選択</h1>
       <div
         onClick={onSubmit}
-        className="cursor-pointer hover:bg-red-300 bg-red-400 rounded-full p-4 transition duration-700 flex items-center justify-center"
+        className="cursor-pointer hover:bg-red-300 bg-red-400 text-white rounded-full p-4 transition duration-700 flex items-center justify-center"
       >
         <img src={playImage} alt="play" className="w-6 h-6" />
+        シミュレーション
       </div>
     </div>
     <div className="bg-gray-100 shadow-md rounded p-4 mb-8">
