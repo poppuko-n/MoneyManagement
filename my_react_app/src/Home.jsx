b4777271b635f7f62f43eb5248eb76a4ae689996
poppuko-n <souta0609@icloud.com>
2025-04-18 21:42:38 +0900

    style: 簡単３ステップの表示をスライドにする

--- my_react_app/src/Home.jsx
+++ my_react_app/src/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
             animate={{ scaleX: 1 }}
             transition={{ duration: 2 }}
             style={{originX: 0}}
-            className="h-[1px] bg-gray-400 mx-auto mt-2"
+            className="h-[1.5px] bg-gray-400 mx-auto mt-2"
           />
         </div>
 
@@ -49,32 +49,47 @@ const Home = () => {
 
         <div className="flex flex-col md:flex-row items-center justify-between gap-16">
 
-          <div className="text-center bg-white shadow-lg rounded-xl p-8 flex-1 max-w-[30%] min-h-[450px]">
+          <motion.div 
+            initial={{ opacity:0, y: 200 }}
+            whileInView={{ opacity:1, y:0 }}
+            transition={{ duration:0.5, ease: "linear" }}
+            viewport={{ once:true }}
+            className="text-center bg-white shadow-lg rounded-xl p-8 flex-1 max-w-[30%] min-h-[450px]">
             <h3 className="text-3xl font-extrabold mb-4 text-blue-600">STEP 1</h3>
             <h4 className="text-2xl font-semibold mb-4">家計簿</h4>
             <p className="text-gray-700 text-base mb-8">
               毎月の収支額を把握するために、家計簿機能を提供。自分の資金を把握しましょう。
             </p>
             <img src={step1} alt="STEP 1" className="mx-auto w-52 h-52 object-contain" />
-          </div>
+          </motion.div>
 
-          <div className="text-center bg-white shadow-lg rounded-xl p-8 flex-1 max-w-[30%] min-h-[450px]">
+          <motion.div 
+            initial={{ opacity:0, y: 200 }}
+            whileInView={{ opacity:1, y:0 }}
+            transition={{ duration:0.5, ease: "linear", delay:0.5 }}
+            viewport={{ once:true }}
+            className="text-center bg-white shadow-lg rounded-xl p-8 flex-1 max-w-[30%] min-h-[450px]">
             <h3 className="text-3xl font-extrabold mb-4 text-blue-600">STEP 2</h3>
             <h4 className="text-2xl font-semibold mb-4">銘柄選択</h4>
             <p className="text-gray-700 text-base mb-8">
               実際の銘柄を活用してシミュレーション。自分の余裕資金内で銘柄を選択してみましょう。
             </p>
             <img src={step2} alt="STEP 2" className="mx-auto w-52 h-52 object-contain" />
-          </div>
+          </motion.div>
 
-          <div className="text-center bg-white shadow-lg rounded-xl p-8 flex-1 max-w-[30%] min-h-[450px]">
+          <motion.div 
+            initial={{ opacity:0, y: 200 }}
+            whileInView={{ opacity:1, y:0 }}
+            transition={{ duration:0.5, ease: "linear", delay:1.0 }}
+            viewport={{ once:true }}
+            className="text-center bg-white shadow-lg rounded-xl p-8 flex-1 max-w-[30%] min-h-[450px]">
             <h3 className="text-3xl font-extrabold mb-4 text-blue-600">STEP 3</h3>
             <h4 className="text-2xl font-semibold mb-4">シミュレーション</h4>
             <p className="text-gray-700 text-base mb-8">
               運用期間、積立期間に応じた損益額が表示。今後の投資戦略を練りましょう。
             </p>
             <img src={step3} alt="STEP 3" className="mx-auto w-52 h-52  object-contain" />
-          </div>
+          </motion.div>
 
         </div>
       </div>
