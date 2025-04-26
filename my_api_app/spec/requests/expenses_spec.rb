b39dce79800d6f19ae9ce888af105ae29ef3cc70
poppuko-n <souta0609@icloud.com>
2025-04-26 22:37:55 +0900

    expenseのpostリクエストスペックを実装

--- my_api_app/spec/requests/expenses_spec.rb
+++ my_api_app/spec/requests/expenses_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Expenses", type: :request do
-  describe "GET /index" do
-    let!(:user1) { create(:user1) }
+  let!(:user1) { create(:user1) }
 
-    let!(:food) { create(:food) }
-    let!(:salary) { create(:salary) }
+  let!(:food) { create(:food) }
+  let!(:salary) { create(:salary) }
 
-    let!(:food_log) { create(:food_log, category: food,  user: user1) }
-    let!(:salary_log) { create(:salary_log, category: salary, user: user1) }
+  let!(:food_log) { create(:food_log, category: food,  user: user1) }
+  let!(:salary_log) { create(:salary_log, category: salary, user: user1) }
 
-    let(:headers) { { "Authorization" => "Bearer #{token}" } }
-    let(:token) { generate_token(user1) }
+  let(:headers) { { "Authorization" => "Bearer #{token}" } }
+  let(:token) { generate_token(user1) }
 
+  describe "GET /expenses" do
     context 'ログインユーザーがアクセスした時' do
       it '自分のログを取得することができる' do
         get '/expenses', headers: headers
@@ -29,6 +29,35 @@ RSpec.describe "Expenses", type: :request do
           "amount" => food_log.amount,
           "category_name" => food.name
         )
+      end
+    end
+
+    describe 'POST /expenses' do
+      context '有効なパラメーターの場合' do
+        it 'ログを登録することができる' do
+          expect {
+            post '/expenses', params: {
+              expense: {
+                category_id: food.id,
+                date: Date.today,
+                item: "昼食",
+                amount: 1000
+              }
+            },headers: headers
+          }.to change(ExpenseLog, :count).by(1)
+
+          expect(response).to have_http_status(201)
+
+          json = JSON.parse(response.body)
+          expect(json).to include(
+            "category_id" => food.id,
+            "date" => Date.today.to_s,
+            "item" => "昼食",
+            "amount" => 1000,
+            "user_id" => user1.id
+          )
+
+        end
       end
     end
   end
