ea57ba06e93d368dba979e37cd9a4ad246bc1378
poppuko-n <souta0609@icloud.com>
2025-04-26 12:37:11 +0900

    refactor: expense_logモデルスペックのインスタンス変数をletへ修正

--- my_api_app/spec/models/expense_log_spec.rb
+++ my_api_app/spec/models/expense_log_spec.rb
@@ -1,22 +1,32 @@
 RSpec.describe ExpenseLog, type: :model do
-  before do
-    @user1 = create(:user1)
-    @user2 = create(:user2)
+  let!(:user1) { create(:user1) }
+  let!(:user2) { create(:user2) }
 
-    @food_log = create(:food_log, user: @user1)
-    @rent_log = create(:rent_log, user: @user2)
-    @salary_log = create(:salary_log, user: @user1)
-    @bonus_log = create(:bonus_log, user: @user2)
-  end
+  let!(:food_log) { create(:food_log, user: user1) }
+  let!(:salary_log) { create(:salary_log, user: user1) }
+  let!(:rent_log) { create(:rent_log, user: user2) }
+  let!(:bonus_log) { create(:bonus_log, user: user2) }
 
   describe '::fetch_all_expenses_for_user' do
-    subject {ExpenseLog.fetch_all_expenses_for_user(@user1)}
+    subject { ExpenseLog.fetch_all_expenses_for_user(user1.id) }
     it '指定したユーザーのみのログを返す' do
-      expect(subject.map(&:user_id).uniq).to eq([@user1.id])
+      expect(subject).to all(have_attributes(user_id: user1.id))
     end
 
     it '全件のログを返す' do
       expect(subject.size).to eq(2)
     end
   end
+
+  describe '::fetch_expense_for_user_byid' do
+    subject { ExpenseLog.fetch_expense_for_user_byid(user1.id, food_log.id) }
+    it '指定したログのみを返す' do
+      expect(subject).to  have_attributes(user_id: user1.id, id: food_log.id)
+    end
+  end
+
+  # describe 'バリデーションのテスト' do
+
+  # end
+
 end
