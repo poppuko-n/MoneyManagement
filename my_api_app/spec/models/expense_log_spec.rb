20b6ee2f98b0d245c7cfa7496bc3650f615c3f75
poppuko-n <souta0609@icloud.com>
2025-04-26 17:09:21 +0900

    expenseモデルスペックの実装

--- my_api_app/spec/models/expense_log_spec.rb
+++ my_api_app/spec/models/expense_log_spec.rb
@@ -2,10 +2,15 @@ RSpec.describe ExpenseLog, type: :model do
   let!(:user1) { create(:user1) }
   let!(:user2) { create(:user2) }
 
-  let!(:food_log) { create(:food_log, user: user1) }
-  let!(:salary_log) { create(:salary_log, user: user1) }
-  let!(:rent_log) { create(:rent_log, user: user2) }
-  let!(:bonus_log) { create(:bonus_log, user: user2) }
+  let!(:food) {create(:food)}
+  let!(:salary) {create(:salary)}
+  let!(:rent) {create(:rent)}
+  let!(:bonus) {create(:bonus)}
+
+  let!(:food_log) { create(:food_log, category: food,  user: user1) }
+  let!(:salary_log) { create(:salary_log, category: salary, user: user1) }
+  let!(:rent_log) { create(:rent_log, category: rent, user: user2) }
+  let!(:bonus_log) { create(:bonus_log, category: bonus, user: user2) }
 
   describe '::fetch_all_expenses_for_user' do
     subject { ExpenseLog.fetch_all_expenses_for_user(user1.id) }
@@ -25,7 +30,60 @@ RSpec.describe ExpenseLog, type: :model do
     end
   end
 
-  # describe 'バリデーションのテスト' do
+  describe 'バリデーションのテスト' do
+    subject { ExpenseLog.new(category_id: category_id, user_id: user_id, date: date, item: item, amount: amount) }
+    let (:category_id) { food.id }
+    let (:user_id) { user1.id }
+    let (:date) { Date.today}
+    let (:item) { "夜食" }
+    let (:amount) { 5000 }
 
-  # end
+    context '正常系' do
+      it '有効なカテゴリである' do
+        expect(subject).to be_valid
+      end
+    end
+
+    context '異常系' do
+      context 'dateが未入力' do
+        let(:date) { '' }
+        it 'valid?メソッドがfalseを返し、errorsに「入力してください」と格納されること' do
+          expect(subject).not_to be_valid
+          expect(subject.errors[:date]).to include('を入力してください')
+        end
+      end
+
+      context 'itemが未入力' do
+        let(:item) { '' }
+        it 'valid?メソッドがfalseを返し、errorsに「入力してください」と格納されること' do
+          expect(subject).not_to be_valid
+          expect(subject.errors[:item]).to include('を入力してください')
+        end
+      end
+
+      context 'amountが未入力' do
+        let(:amount) { '' }
+        it 'valid?メソッドがfalseを返し、errorsに「入力してください」と格納されること' do
+          expect(subject).not_to be_valid
+          expect(subject.errors[:amount]).to include('を入力してください')
+        end
+      end
+
+      context 'amountが数値でない場合' do
+        let(:amount) { 'test' }
+        it 'valid?メソッドがfalseを返し、errorsに「数値で入力してください」と格納されること' do
+          expect(subject).not_to be_valid
+          expect(subject.errors[:amount]).to include('は数値で入力してください')
+        end
+      end
+
+      context 'amountが整数でない場合' do
+        let(:amount) { 0 }
+        it 'valid?メソッドがfalseを返し、errorsに「より大きい値にしてください」と格納されること' do
+          expect(subject).not_to be_valid
+          expect(subject.errors[:amount]).to include('0より大きい値にしてください')
+        end
+      end
+    end
+  end
 end
