977d36de90563ea318acdc79b33be8e80411f102
poppuko-n <souta0609@icloud.com>
2025-04-26 16:53:57 +0900

    refactor: factorybotの修正

--- my_api_app/spec/factories/expenselogs.rb
+++ my_api_app/spec/factories/expenselogs.rb
@@ -1,27 +1,23 @@
 FactoryBot.define do
   factory :food_log, class: 'ExpenseLog' do
-    association :category, factory: :food
     date { Date.today }
     item { '昼食' }
     amount { 1000 }
   end
 
   factory :rent_log, class: 'ExpenseLog' do
-    association :category, factory: :rent
     date { Date.today }
     item { '家賃' }
     amount { 10000 }
   end
 
   factory :salary_log, class: 'ExpenseLog' do
-    association :category, factory: :salary
     date { Date.today }
     item { '給与' }
     amount { 200000 }
   end
 
   factory :bonus_log, class: 'ExpenseLog' do
-    association :category, factory: :bonus
     date { Date.today }
     item { 'ボーナス' }
     amount { 400000 }
