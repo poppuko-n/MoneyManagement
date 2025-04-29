96feb15b18adaf0f75655b9122aff85ce0584fee
poppuko-n <souta0609@icloud.com>
2025-04-29 13:27:28 +0900

    refactor: aggregate_failuresを使用して複数のexpectをまとめて評価

--- my_api_app/spec/requests/expenses_spec.rb
+++ my_api_app/spec/requests/expenses_spec.rb
@@ -13,16 +13,18 @@ RSpec.describe "Expenses", type: :request do
     context 'ログインユーザーがアクセスした時' do
       it '自分のログを取得することができる' do
         get '/expenses', headers: headers
-        expect(response).to have_http_status(:ok)
-        expect(response.parsed_body.size).to eq(2)
-        expect(response.parsed_body.first).to include(
-          "id" => food_log.id,
-          "transaction_type" => food.transaction_type,
-          "date" => food_log.date.to_s,
-          "item" => food_log.item,
-          "amount" => food_log.amount,
-          "category_name" => food.name
-        )
+        aggregate_failures do
+          expect(response).to have_http_status(:ok)
+          expect(response.parsed_body.size).to eq(2)
+          expect(response.parsed_body.first).to include(
+            "id" => food_log.id,
+            "transaction_type" => food.transaction_type,
+            "date" => food_log.date.to_s,
+            "item" => food_log.item,
+            "amount" => food_log.amount,
+            "category_name" => food.name
+          )
+        end
       end
     end
   end
@@ -30,50 +32,54 @@ RSpec.describe "Expenses", type: :request do
   describe 'POST /expenses' do
     context '有効なパラメーターの場合' do
       it 'ログを登録することができる' do
-        expect {
-          post '/expenses', params: {
-            expense: {
-              category_id: food.id,
-              date: Date.today,
-              item: "昼食",
-              amount: 1000
-            }
-          }, headers: headers
-        }.to change(ExpenseLog, :count).by(1)
-
-        expect(response).to have_http_status(201)
-        expect(response.parsed_body).to include(
-          "category_id" => food.id,
-          "date" => Date.today.to_s,
-          "item" => "昼食",
-          "amount" => 1000,
-          "user_id" => user1.id
-        )
+        aggregate_failures do
+          expect {
+            post '/expenses', params: {
+              expense: {
+                category_id: food.id,
+                date: Date.today,
+                item: "昼食",
+                amount: 1000
+              }
+            }, headers: headers
+          }.to change(ExpenseLog, :count).by(1)
+  
+          expect(response).to have_http_status(201)
+          expect(response.parsed_body).to include(
+            "category_id" => food.id,
+            "date" => Date.today.to_s,
+            "item" => "昼食",
+            "amount" => 1000,
+            "user_id" => user1.id
+          )
+        end
       end
     end
 
     context '無効なパラメーターの場合' do
       it 'ログを登録することができない' do
-        expect {
-          post '/expenses', params: {
-            expense: {
-              category_id: '',
-              date: '',
-              item: '',
-              amount: ''
-            }
-          }, headers: headers
-        }.not_to change(ExpenseLog, :count)
-
-        expect(response).to have_http_status(422)
-        expected_errors = [
-          'カテゴリー を入力してください',
-          '日付 を入力してください',
-          '内容 を入力してください',
-          '金額 を入力してください',
-          '金額 は数値で入力してください'
-        ]
-        expect(response.parsed_body['errors']).to eq(expected_errors)
+        aggregate_failures do
+          expect {
+            post '/expenses', params: {
+              expense: {
+                category_id: '',
+                date: '',
+                item: '',
+                amount: ''
+              }
+            }, headers: headers
+          }.not_to change(ExpenseLog, :count)
+  
+          expect(response).to have_http_status(422)
+          expected_errors = [
+            'カテゴリー を入力してください',
+            '日付 を入力してください',
+            '内容 を入力してください',
+            '金額 を入力してください',
+            '金額 は数値で入力してください'
+          ]
+          expect(response.parsed_body['errors']).to eq(expected_errors)
+        end
       end
     end
   end
@@ -89,12 +95,14 @@ RSpec.describe "Expenses", type: :request do
             amount: 2000
           }
         }, headers: headers
-        expect(response).to have_http_status(:ok)
-        expect(response.parsed_body['item']).to eq('更新された昼食')
-        expect(response.parsed_body['amount']).to eq(2000)
-        food_log.reload
-        expect(food_log.item).to eq('更新された昼食')
-        expect(food_log.amount).to eq(2000)
+        aggregate_failures do
+          expect(response).to have_http_status(:ok)
+          expect(response.parsed_body['item']).to eq('更新された昼食')
+          expect(response.parsed_body['amount']).to eq(2000)
+          food_log.reload
+          expect(food_log.item).to eq('更新された昼食')
+          expect(food_log.amount).to eq(2000)
+        end
       end
     end
 
@@ -108,11 +116,13 @@ RSpec.describe "Expenses", type: :request do
             amount: 2000
           }
         }, headers: headers
-        expect(response).to have_http_status(422)
-        expect(response.parsed_body['errors']).to eq(["内容 を入力してください"])
-        food_log.reload
-        expect(food_log.item).to eq('昼食')
-        expect(food_log.amount).to eq(1000)
+        aggregate_failures do
+          expect(response).to have_http_status(422)
+          expect(response.parsed_body['errors']).to eq(["内容 を入力してください"])
+          food_log.reload
+          expect(food_log.item).to eq('昼食')
+          expect(food_log.amount).to eq(1000)
+        end
       end
     end
 
@@ -134,21 +144,25 @@ RSpec.describe "Expenses", type: :request do
   describe 'DELETE /expenses/:id' do
     context 'ログが存在する場合' do
       it 'ログを削除することができる' do
-        expect {
-          delete "/expenses/#{food_log.id}", headers: headers
-        }.to change(ExpenseLog, :count).by(-1)
-
-        expect(response).to have_http_status(204)
-        expect(ExpenseLog.exists?(food_log.id)).to be false
+        aggregate_failures do
+          expect {
+            delete "/expenses/#{food_log.id}", headers: headers
+          }.to change(ExpenseLog, :count).by(-1)
+  
+          expect(response).to have_http_status(204)
+          expect(ExpenseLog.exists?(food_log.id)).to be false
+        end
       end
     end
 
     context 'ログが存在しない場合' do
       it 'ログを削除することができない' do
-        expect {
-          delete "/expenses/#{food_log.id+100}", headers: headers
-        }.not_to change(ExpenseLog, :count)
-        expect(response).to have_http_status(404)
+        aggregate_failures do
+          expect {
+            delete "/expenses/#{food_log.id+100}", headers: headers
+          }.not_to change(ExpenseLog, :count)
+          expect(response).to have_http_status(404)
+        end
       end
     end
   end
