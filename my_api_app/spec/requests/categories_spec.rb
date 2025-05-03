9e2b47652ef080be4508ee5f2d19a4ec0e79c3ec
poppuko-n <souta0609@icloud.com>
2025-05-03 22:28:58 +0900

    test: カテゴリ一覧APIの種別ごとの取得に関するリクエストスペックを追加

--- my_api_app/spec/requests/categories_spec.rb
+++ my_api_app/spec/requests/categories_spec.rb
@@ -1,7 +1,18 @@
-require 'rails_helper'
-
 RSpec.describe "Categories", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /categories" do
+    let!(:food) { create(:category, name: '食費', transaction_type: '支出') }
+    let!(:salary) { create(:category, name: '給料', transaction_type: '収入') }
+    it '種別毎（支出、収入）のカテゴリを取得することができる' do
+      get '/categories'
+      aggregate_failures do
+        expect(response).to have_http_status(:ok)
+
+        response_body = response.parsed_body
+        expect(response_body['expense_categories'][0]['name']).to eq('食費')
+        expect(response_body['expense_categories'][0]['transaction_type']).to eq('支出')
+        expect(response_body['income_categories'][0]['name']).to eq('給料')
+        expect(response_body['income_categories'][0]['transaction_type']).to eq('収入')
+      end
+    end
   end
 end
