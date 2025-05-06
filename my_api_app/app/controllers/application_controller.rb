b1bb8ab4f10f211b0819a95373a0dce5e284c66a
poppuko-n <souta0609@icloud.com>
2025-05-06 14:02:55 +0900

    refactor: ApplicationControllerにコメント追記と責務の分離

--- my_api_app/app/controllers/application_controller.rb
+++ my_api_app/app/controllers/application_controller.rb
@@ -1,18 +1,22 @@
+# JWTを用いたユーザー認証に関する以下の処理を担う。
+# - JWTトークンによるユーザー認証（authenticate_user）:
+#     - リクエストヘッダーからトークンを取得・デコードし、@current_user を設定。
+#     - 家計簿ログなど、ユーザーを特定して操作するAPIで使用。
+# - JWTトークンの生成（create_token）:
+#     - ログイン、新規登録時にトークンを発行し、クライアントへ返却。
+
 class ApplicationController < ActionController::API
   def authenticate_user
-    authorization_header = request.headers[:Authorization]
-    unless authorization_header
-      return render_unauthorized("ログインが必要です。")
-    end
+    return render_unauthorized("ログインが必要です。") unless authorization_header?
 
-    token = authorization_header.split(" ")[1]
+    token = request.headers[:Authorization].split(" ")[1]
     secret_key = Rails.application.credentials.secret_key_base
 
     begin
+      # 実例 decoded_code　= [{"user_id"=>1}, {"alg"=>"HS256"}]
       decoded_code = JWT.decode(token, secret_key)
       @current_user = User.find(decoded_code[0]["user_id"])
-    rescue
-      ActiveRecord::RecordNotFound
+    rescue ActiveRecord::RecordNotFound
       render_unauthorized("ユーザーが見つかりません。")
     rescue JWT::DecodeError => e
       render_unauthorized("#{e.message}")
@@ -27,7 +31,11 @@ class ApplicationController < ActionController::API
   end
 
   private
-  def render_unauthorized(message = "Unauthorized")
+  def authorization_header?
+    request.headers[:Authorization].present?
+  end
+
+  def render_unauthorized(message)
     render json: { errors: [ message ] }, status: :unauthorized
   end
 end
