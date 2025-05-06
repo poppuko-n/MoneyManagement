8c0d588c55f52176b0c0a6301ed37a22493b0440
poppuko-n <souta0609@icloud.com>
2025-05-06 16:31:12 +0900

    refactor: UserControllerにコメント追記と改行を修正

--- my_api_app/app/controllers/users_controller.rb
+++ my_api_app/app/controllers/users_controller.rb
@@ -1,25 +1,23 @@
 # 認証にはJWTを使用し、ApplicationControllerに定義された `create_token` を活用。
 
 class UsersController < ApplicationController
+  # POST /users
   def create
     user = User.new(user_params)
     if user.save
       render_with_token(user, :created)
     else
-      render json: {
-        errors: user.errors.full_messages
-        }, status: :unprocessable_entity
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
+  # POST /login
   def login
     user = User.find_by(name: params[:name])
     if user && user.authenticate(params[:password])
       render_with_token(user, :ok)
     else
-      render json: {
-        errors: [ "名前またはパスワードが違います。" ]
-        }, status: :unauthorized
+      render json: { errors: [ "名前またはパスワードが違います。" ] }, status: :unauthorized
     end
   end
 
@@ -30,7 +28,6 @@ class UsersController < ApplicationController
   end
 
   def render_with_token(user, status)
-    token = create_token(user.id)
-    render json: { token: token }, status: status
+    render json: { token: create_token(user.id) }, status: status
   end
 end
