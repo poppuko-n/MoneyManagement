df9a2e51a09c5af9362faee0f233855038bf6b0f
poppuko-n <souta0609@icloud.com>
2025-05-07 21:26:23 +0900

    refactor: Categoryモデルの簡素化

--- my_api_app/app/controllers/categories_controller.rb
+++ my_api_app/app/controllers/categories_controller.rb
@@ -2,8 +2,8 @@ class CategoriesController < ApplicationController
   # GET /categories
   def index
     render json: {
-      expense_categories: Category.search(transaction_type: "支出"),
-      income_categories: Category.search(transaction_type: "収入")
+      expense_categories: Category.where(transaction_type: "支出"),
+      income_categories: Category.where(transaction_type: "収入")
     }
   end
 end
