504d4889533ec2dce81d17fa50f6dc9e61e7cad6
poppuko-n <souta0609@icloud.com>
2025-05-18 11:35:12 +0900

    refactor: expense_logsテーブルのdateおよびamountカラムにNOT NULL制約を追加

--- my_api_app/db/schema.rb
+++ my_api_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_18_021043) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_18_023236) do
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -33,9 +33,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_18_021043) do
 
   create_table "expense_logs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "category_id", null: false
-    t.date "date"
+    t.date "date", null: false
     t.string "item", null: false
-    t.bigint "amount"
+    t.bigint "amount", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
