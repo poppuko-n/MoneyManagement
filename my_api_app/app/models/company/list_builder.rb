2b81011cd3778f2561397635fabc3868abec4faf
poppuko-n <souta0609@icloud.com>
2025-06-11 23:15:24 +0900

    refactor: 前日からの増減額を削除し、コードを整形

--- my_api_app/app/models/company/list_builder.rb
+++ my_api_app/app/models/company/list_builder.rb
@@ -11,24 +11,12 @@ class Company
       private
 
       def build_with_diff_price(company)
-        latest, second = fetch_latest_prices(company)
-        diff = latest - second
-
         {
           code: company.code,
           name: company.name,
           sector_name: company.sector.name,
-          latest_price: latest,
-          price_difference: diff,
-          price_difference_rate: format('%.2f', diff.to_f/latest)
+          latest_price: company.latest_stock_price
         }
-      end
-
-      def fetch_latest_prices(company)
-        StockPrice.where(company_code: company.code)
-                  .order(date: :desc)
-                  .limit(2)
-                  .pluck(:close_price)
       end
     end
   end
