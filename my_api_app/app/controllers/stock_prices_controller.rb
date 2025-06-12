345220b5c65bf607394114c4858cfeb7a00a38cb
poppuko-n <souta0609@icloud.com>
2025-06-13 07:33:13 +0900

    refactor: シミュレーション結果の整形をコントローラーへ移行

--- my_api_app/app/controllers/stock_prices_controller.rb
+++ my_api_app/app/controllers/stock_prices_controller.rb
@@ -8,6 +8,18 @@ class StockPricesController < ApplicationController
   # POST /stock_prices/simulate
   def simulate
     # params[:data] = [{"code"=>銘柄コード, "name"=>"銘柄名", "price"=>現在価格, "quantity"=>購入数量}]
-    render json: StockPrice::SimulationResultBuilder.call(params[:data]), status: :ok
+    render json: build_simulation_result(params[:data]), status: :ok
+  end
+
+  private
+
+  def build_simulation_result(investment_simulation_targets)
+        results = investment_simulation_targets.map do |target|
+        StockPrice::InvestmentSimulator.call(target)
+      end
+      {
+        results: results,
+        ai_analysis: StockPrice::AiAnalyzer.call(results)
+      }
   end
 end
