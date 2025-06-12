72e273b100aa0f34de84ef166b411287e2a1cc0a
poppuko-n <souta0609@icloud.com>
2025-06-13 07:40:22 +0900

    refactor: investment_simulationロジックをstockpriceコントローラーへ移行

--- my_api_app/app/controllers/stock_prices_controller.rb
+++ my_api_app/app/controllers/stock_prices_controller.rb
@@ -1,4 +1,5 @@
 class StockPricesController < ApplicationController
+  TARGET_PERIODS = (1..12).to_a
   # POST /stock_prices/update
   def update
     StockPrice::Importer.call
@@ -14,12 +15,99 @@ class StockPricesController < ApplicationController
   private
 
   def build_simulation_result(investment_simulation_targets)
-        results = investment_simulation_targets.map do |target|
-        StockPrice::InvestmentSimulator.call(target)
+      results = investment_simulation_targets.map do |target|
+        build_investment_simuration(target)
       end
       {
         results: results,
         ai_analysis: StockPrice::AiAnalyzer.call(results)
       }
+  end
+
+# 返すJSONは,以下の通り（３ヶ月の場合の例）。
+# "code": "83060",
+# "name": "三菱ＵＦＪフィナンシャル・グループ",
+# "current_price": 2178,
+# "quantity": 1,
+# "one_time": [
+#   { "period": "1_month", "value": 2885, "deposit": 2178 }
+#   { "period": "2_month", "value": 2505, "deposit": 2178 }
+#   ...
+#   { "period": "12_month", "value": 2964, "deposit": 2178 }
+# ],
+# "accumulated": [
+#   { "period": "1_month", "value": 2396, "deposit": 2178 }
+#   { "period": "2_month", "value": 4792, "deposit": 4356 }
+#   ...
+#   { "period": "12_month", "value": 28750, "deposit": 26136 }
+# ]
+  def build_investment_simuration(target)
+    target_company_code, target_company_name, current_price, quantity  = target.values_at(:code, :name, :price, :quantity)
+        {
+          code: target_company_code,
+          name: target_company_name,
+          current_price: current_price,
+          quantity: quantity,
+          one_time: format_one_time_simulations(target_company_code, current_price, quantity),
+          accumulated: format_accumulated_simulations(target_company_code, current_price, quantity)
+        }
+  end
+
+  def format_one_time_simulations(code, current_price, quantity)
+    TARGET_PERIODS.map do |month|
+      past_price = fetch_past_price(code, month)
+      { period: "#{month}_month",
+        value: (current_price**2 / past_price) * quantity,
+        deposit: current_price * quantity }
+    end
+  end
+
+  def fetch_past_price(code, n)
+    start_date = Date.today
+    end_date = start_date - n.months
+
+    price = StockPrice.where(company_code: code, date: end_date..start_date)
+                      .order(date: :asc)
+                      .first&.close_price
+  end
+
+  def format_accumulated_simulations(code, current_price, quantity)
+    average_growth_rate = calculate_average_growth_rate(code, current_price)
+    monthly_deposit = current_price * quantity
+
+    # 各月の積立分に対して、それぞれ運用月数に応じた複利を掛けて合計する
+    # 月=3 の場合、以下のように計算
+    # - 1ヶ月目に投資した分 → 3ヶ月運用されている → r^3倍
+    # - 2ヶ月目に投資した分 → 2ヶ月運用されている → r^2倍
+    # - 3ヶ月目に投資した分 → 1ヶ月運用されている → r^1倍
+    TARGET_PERIODS.map do |month|
+      total_value = (0...month).sum do |i|
+        monthly_deposit * (average_growth_rate ** (month-i))
+      end
+
+      {
+        period: "#{month}_month",
+        value: total_value.round,
+        deposit: monthly_deposit * month
+      }
+    end
+  end
+
+  def fetch_past_average_price(code, month)
+      start_date = Date.today - month.months
+      end_date = start_date + 30.days
+
+      StockPrice.where(company_code: code, date: start_date..end_date)
+                .average(:close_price)
+  end
+
+  def calculate_average_growth_rate(code, current_price)
+    rates = TARGET_PERIODS.map do |i|
+      from = fetch_past_average_price(code, i)
+      to   = i == 1 ? current_price : fetch_past_average_price(code, i - 1)
+      to / from
+    end
+
+    (rates.sum / rates.size).ceil(2)
   end
 end
