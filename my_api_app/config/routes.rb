0caa1688581ec0a37012c87cac7907db1f952fb1
poppuko-n <souta0609@icloud.com>
2025-06-15 07:25:06 +0900

    refactor: ルーティングのonlyオプションを%i記法に統一

--- my_api_app/config/routes.rb
+++ my_api_app/config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   root to: proc { [ 200, {}, [ "OK" ] ] }
 
-  # get "expense_logs/export", to: "expense_logs#export"
-
   post "stock_prices/simulate", to: "stock_prices#simulate"
   post "stock_prices/update", to: "stock_prices#update"
 
-  resources :companies, only: [ :index ]
-  resources :expense_logs, only: [ :index, :show, :create, :update, :destroy ]
-  resources :categories, only: [ :index ]
-  resources :users, only: [ :create ]
+  resources :companies, only: %i[index]
+  resources :categories, only: %i[index]
+  resources :expense_logs, only: %i[index show create update destroy]
+  resources :users, only: %i[create]
 
   post "/login", to: "users#login"
 end
