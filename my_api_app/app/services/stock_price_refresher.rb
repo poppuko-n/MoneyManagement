e7c1d46d0fe1d31ebda8170698b240c3ebfcdc16
poppuko-n <souta0609@icloud.com>
2025-06-19 07:49:05 +0900

    refactor: StockPriceRefresherをシンプル化しimport!で例外を発生させるよう修正

--- my_api_app/app/services/stock_price_refresher.rb
+++ my_api_app/app/services/stock_price_refresher.rb
@@ -1,32 +1,18 @@
 class StockPriceRefresher
   def  call
-    stock_prices = target_codes.flat_map { |code| format_stock_prices(code) }
-    update_company_stock_prices(stock_prices)
+    stock_prices = Company.pluck(:code).flat_map { |code| format_stock_prices(code) }
+    StockPrice.import! stock_prices, on_duplicate_key_update: [ :close_price ]
   end
 
   private
 
-  def target_codes
-    Company.pluck(:code)
-  end
-
   def format_stock_prices(code)
-    JquantsClient.new.fetch_daily_quotes(code).map do |quote|
+    JquantsClient.new.fetch_daily_quotes(code).map { |quote|
       StockPrice.new(
         company_code: quote["Code"],
         date: quote["Date"],
         close_price: quote["Close"]
       )
-    end
-  end
-
-  def update_company_stock_prices(stock_prices)
-    valid, invalid = stock_prices.partition(&:valid?)
-    log_errors(invalid) if invalid.any?
-    StockPrice.import valid, on_duplicate_key_update: [ :close_price ]
-  end
-
-  def log_errors(records)
-    records.each { |record| puts record.errors.full_messages }
+    }
   end
 end
