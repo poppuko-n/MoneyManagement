590588b16a075034648116a1e2eee06d3f7dff90
poppuko-n <souta0609@icloud.com>
2025-06-28 01:00:20 +0900

    refactor: 家計簿リストのリファクタリング

--- my_react_app/src/ExpenseList.jsx
+++ my_react_app/src/ExpenseList.jsx
@@ -3,12 +3,13 @@ import deleteImage from "./assets/delete.svg";
 import arrowImage from "./assets/arrow.svg";
 import ExpenseApi from './lib/ExpenseApi';
 
-const ExpenseDetail =  ({ expenses, onSelectExpense, onBack }) => {
-  const handleDelete = (expense_id) => {
+const ExpenseList =  ({ expenses, setCurrentExpenseId, onBack }) => {
+  const handleDelete = async(expense_id) => {
     if (!window.confirm("本当に削除しますか")) return;
 
     try{
-      ExpenseApi.deleteExpenseLog(expense_id);
+      await ExpenseApi.deleteExpenseLog(expense_id);
+      alert("削除しました。")
       onBack();
     } catch {
       alert("削除に失敗しました。再度お試しください。");
@@ -16,31 +17,31 @@ const ExpenseDetail =  ({ expenses, onSelectExpense, onBack }) => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h2 className="text-xl font-bold text-center mb-4 mt-08">収支一覧</h2>
+    <div className="container mx-auto">
+      <p className="font-bold text-center mb-4">収支一覧</p>
 
-      <div className='flex justify-end mb-4'>
+      <div className='flex justify-end mb-2'>
         <button onClick={onBack} className='flex items-center'>
           グラフへ戻る
-          <img src={arrowImage} alt="arrow" className='w-5 h-5 ml-2' />
+          <img src={arrowImage} alt="arrow" className='w-4 h-4 ml-2' />
         </button>
       </div>
 
-      <table className="min-w-full border border-gray-300 bg-white">
+      <table className="w-full">
         <thead>
-          <tr className="bg-gray-100">
-            <th className="border px-4 py-2 text-left">日付</th>
-            <th className="border px-4 py-2 text-left">種類</th>
-            <th className="border px-4 py-2 text-left">カテゴリ</th>
-            <th className="border px-4 py-2 text-left">項目</th>
-            <th className="border px-4 py-2 text-right w-40">金額(円)</th>
+          <tr className="border font-bold bg-gray-100 text-left">
+            <th className="border px-4 py-2">日付</th>
+            <th className="border px-4 py-2">種類</th>
+            <th className="border px-4 py-2">カテゴリ</th>
+            <th className="border px-4 py-2">項目</th>
+            <th className="border px-4 py-2 text-right w-40">金額</th>
             <th className="border px-4 py-2 text-center w-20">編集</th>
             <th className="border px-4 py-2 text-center w-20">削除</th>
           </tr>
         </thead>
         <tbody>
           {expenses.map((expense) => (
-            <tr key={expense.id} className="hover:bg-gray-50 transition duration-200">
+            <tr key={expense.id}>
               <td className="border px-4 py-2">{expense.date}</td>
               <td
                 className={`border px-4 py-2 font-bold ${
@@ -57,24 +58,22 @@ const ExpenseDetail =  ({ expenses, onSelectExpense, onBack }) => {
                 {expense.amount.toLocaleString()}
               </td>
               <td className="border px-2 py-1 text-center">
-                <div className="flex justify-center items-center h-full">
+                <button onClick={() => setCurrentExpenseId(expense.id)}>
                   <img
                     src={editImage}
                     alt="edit"
-                    className="cursor-pointer hover:bg-gray-300 rounded p-1 transition duration-700"
-                    onClick={() => onSelectExpense(expense.id)}
+                    className="hover:bg-gray-300 p-1"
                   />
-                </div>
+                </button>
               </td>
               <td className="border px-2 py-1 text-center">
-                <div className="flex justify-center items-center h-full">
+                <button onClick={() => handleDelete(expense.id)}>
                   <img
                     src={deleteImage}
                     alt="delete"
-                    className="cursor-pointer hover:bg-red-100 rounded p-1 transition duration-700"
-                    onClick={() => handleDelete(expense.id)}
+                    className="inline-block hover:bg-red-100 p-1"
                   />
-                </div>
+                </button>
               </td>
             </tr>
           ))}
@@ -83,4 +82,4 @@ const ExpenseDetail =  ({ expenses, onSelectExpense, onBack }) => {
     </div>
   );
 };
-export default ExpenseDetail;
+export default ExpenseList;
