4fe68aecdd3e30a6a9e650c12d26f6146f28c53d
poppuko-n <souta0609@icloud.com>
2025-06-28 16:38:33 +0900

    refactor: returnの削除

--- my_react_app/src/SimulationResult.jsx
+++ my_react_app/src/SimulationResult.jsx
@@ -10,32 +10,27 @@ const SimulationResult = ({projectionResults}) => {
   const [aiAnalysis, setAiAnalysis] = useState(null);
   const [simulationType, setSimulationType] = useState("one_time");
 
-  const ResultsByType = projectionResults.map( r => {
-    const finalResult = r[simulationType].at(-1);
-    return {
-      name: r.name,
-      deposit: finalResult.deposit,
-      value: finalResult.value
-    };
-  });
+  const ResultsByType = projectionResults.map(r => ({
+    name: r.name,
+    deposit: r[simulationType].at(-1).deposit,
+    value: r[simulationType].at(-1).value
+  }));
 
   useEffect(()=>{
     CompanyApi.createProjectionsAnalyses({data: projectionResults})
       .then(response => setAiAnalysis(response.data))
   },[])
 
-// 各月について、全銘柄の同月のシミュレーション結果を合算し、
-// 「○ヶ月後」「預金額（purchase_amount）」「評価額（evaluation_amount）」を持つオブジェクトの配列を返す。
   const getChartData = () =>
-  Array.from({ length: 12 }, (_, i) => {
-    const period = `${i + 1}_month`;
-    const [deposit, value] = projectionResults.reduce(
-      ([d, v], r) => {
-        const sim = r[simulationType].find(s => s.period === period);
-        return [d + sim.deposit, v + +sim.value];
-      },
-      [0, 0]
-    );
+    Array.from({ length: 12 }, (_, i) => {
+      const period = `${i + 1}_month`;
+      const [deposit, value] = projectionResults.reduce(
+        ([d, v], r) => {
+          const sim = r[simulationType].find(s => s.period === period);
+          return [d + sim.deposit, v + +sim.value];
+        },
+        [0, 0]
+      );
 
     return {
       date: `${i + 1}ヶ月後`,
