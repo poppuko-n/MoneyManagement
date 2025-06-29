f8c9f6fa474c3b67b6db60a516fe797920a4ca54
poppuko-n <souta0609@icloud.com>
2025-06-29 13:35:00 +0900

    refacotr: 不要なコンポーネントの削除

--- my_react_app/src/features/simulation/InvestmentAmountDisplay.jsx
+++
@@ -1,9 +0,0 @@
-const InvestmentAmountDisplay = ({ totalAmount }) => {
-  return (
-    <div className="shadow p-4 mb-4">
-      <h2 className="text-xl">合計金額: {totalAmount.toLocaleString()} 円</h2>
-    </div>
-  );
-};
-
-export default InvestmentAmountDisplay;
