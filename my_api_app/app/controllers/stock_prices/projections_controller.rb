f771cbd63a87d544b120c103afcf74dff4269f54
poppuko-n <souta0609@icloud.com>
2025-07-05 20:49:18 +0900

    refactor: ストロングパラメータの作成

--- my_api_app/app/controllers/stock_prices/projections_controller.rb
+++ my_api_app/app/controllers/stock_prices/projections_controller.rb
@@ -1,7 +1,13 @@
 class StockPrices::ProjectionsController < ApplicationController
   # GET /stock_prices/projections
   def index
-    result = ProjectionGenerator.new(params[:data]).call
+    result = ProjectionGenerator.new(projection_params).call
     render json: result
+  end
+
+  private
+
+  def projection_params
+    params.require(:data).map { |i| i.permit(:code, :quantity) }
   end
 end
