5ee821519a03ddb1dcf17f592465b18c2052baf5
poppuko-n <souta0609@icloud.com>
2025-07-05 15:41:42 +0900

    refactor:　命名を修正

--- my_api_app/app/services/one_time_simulator.rb
+++ my_api_app/app/services/one_time_simulator.rb
@@ -1,40 +1,44 @@
 class OneTimeSimulator
   TARGET_PERIODS = (1..12).to_a
 
+  # 一括投資シミュレーションを実行するクラス
+  #
+  # @param quantity [Integer] 投資数量
+  # @param prices [Array<StockPrice>] 
   def initialize(quantity, prices)
     @quantity = quantity
     @prices = prices
   end
 
   def call
-    TARGET_PERIODS.map { |month| to_api(month) }
+    TARGET_PERIODS.map { |month| build_monthly_result(month) }
   end
 
   private
 
-  def fetch_past_price(month)
-    start_date = Date.today - month.months
-    @prices.find { |p| p.date >= start_date }&.close_price
-  end
-
-  def latest_price
-    @prices.max_by(&:date)&.close_price
-  end
-
-  def calculate_one_time_investment_value(past_price)
-    latest_price**2 / past_price * @quantity
-  end
-
-  def calculate_one_time_investment_deposit
-    latest_price * @quantity
-  end
-
-  def to_api(month)
-    past_price = fetch_past_price(month)
+  def build_monthly_result(month)
+    past_price = find_historical_price(month)
     {
       period: "#{month}_month",
-      value: calculate_one_time_investment_value(past_price),
-      deposit: calculate_one_time_investment_deposit
+      value: calculate_investment_value(past_price),
+      deposit: calculate_deposit_amount
     }
+  end
+
+  def find_historical_price(month)
+    start_date = month.months.ago
+    @prices.find { |price| price.date >= start_date }.close_price
+  end
+
+  def calculate_investment_value(past_price)
+    find_current_price**2 / past_price * @quantity
+  end
+
+  def calculate_deposit_amount
+    find_current_price * @quantity
+  end
+
+  def find_current_price
+    @prices.max_by(&:date).close_price
   end
 end
