abc95aa78784b7044fe511666fce6ba68644e0c9
poppuko-n <souta0609@icloud.com>
2025-07-05 18:39:27 +0900

    refactor: 時間計算量の改善

--- my_api_app/app/services/one_time_simulator.rb
+++ my_api_app/app/services/one_time_simulator.rb
@@ -7,38 +7,46 @@ class OneTimeSimulator
   # @param prices [Array<StockPrice>] 
   def initialize(quantity, prices)
     @quantity = quantity
-    @prices = prices
+    @prices = prices.sort_by(&:date)
+    @current_price = @prices.last.close_price
+    @fixed_deposit = @current_price * @quantity
   end
 
   def call
-    TARGET_PERIODS.map { |month| build_monthly_result(month) }
+    monthly_growth_rates = calculate_monthly_growth_rates
+    TARGET_PERIODS.map { |month| build_monthly_result(month, monthly_growth_rates) }
   end
 
   private
 
-  def build_monthly_result(month)
-    past_price = find_historical_price(month)
+  def build_monthly_result(month, monthly_growth_rates)
     {
       period: "#{month}_month",
-      value: calculate_investment_value(past_price),
-      deposit: calculate_deposit_amount
+      value: calculate_investment_value(month, monthly_growth_rates),
+      deposit: @fixed_deposit.round
     }
   end
 
-  def find_historical_price(month)
+  def calculate_investment_value(month, monthly_growth_rates)
+    value = @fixed_deposit
+    month.times do |i|
+      growth_rate = monthly_growth_rates[i]
+      value *= growth_rate
+    end
+    value.round
+  end
+
+  def calculate_monthly_growth_rates
+    monthly_prices = TARGET_PERIODS.map { |month| find_monthly_price(month) }
+    monthly_prices.unshift(@current_price)
+    
+    monthly_prices.each_cons(2).map { |current, previous| current.to_f / previous }.reverse
+  end
+
+
+
+  def find_monthly_price(month)
     start_date = month.months.ago
-    @prices.find { |price| price.date >= start_date }.close_price
-  end
-
-  def calculate_investment_value(past_price)
-    find_current_price**2 / past_price * @quantity
-  end
-
-  def calculate_deposit_amount
-    find_current_price * @quantity
-  end
-
-  def find_current_price
-    @prices.max_by(&:date).close_price
+    @prices.find { |p| p.date >= start_date }&.close_price
   end
 end
