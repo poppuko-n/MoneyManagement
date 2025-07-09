1e7a3b56806fc3ee8bb9d0a280ca745a8aeef854
poppuko-n <souta0609@icloud.com>
2025-07-09 23:12:52 +0900

    test: リクエストスペックの追加

---
+++ my_api_app/spec/requests/stock_prices/refreshes_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "StockPrices::Refreshes", type: :request do
+  describe "POST /stock_prices/refreshes" do
+    before do
+      allow(StockPriceRefresher).to receive(:call)
+    end
+
+    it "StockPriceRefresherが呼ばれて成功メッセージが返される" do
+      post "/stock_prices/refreshes"
+      
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to eq({ "message" => "Stock prices refreshed" })
+      expect(StockPriceRefresher).to have_received(:call)
+    end
+  end
+end 
